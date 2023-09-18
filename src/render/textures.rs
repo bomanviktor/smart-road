640b74bbbfe114b3df048dc612d83267ed8f7f14
FinnTune <adieter@protonmail.com>
2023-09-18 21:46:11 +0300

    Remove `car` from  `Textures` struct and `load` trait.

--- src/render/textures.rs
+++ src/render/textures.rs
@@ -3,7 +3,6 @@ use macroquad::prelude::*;
 pub struct Textures {
     pub road: Texture2D,
     pub bg: Texture2D,
-    pub car: Texture2D,
 }
 
 impl Textures {
@@ -13,9 +12,6 @@ impl Textures {
                 .await
                 .unwrap(),
             bg: macroquad::texture::load_texture("assets/RockBG.png")
-                .await
-                .unwrap(),
-            car: macroquad::texture::load_texture("assets/cars/car.png")
                 .await
                 .unwrap(),
         }
