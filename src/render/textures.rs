828eaf2f66a5bbccd693618f2805b5d99af7aca6
bomanviktor <boman.viktor@gmail.com>
2023-09-22 02:18:14 -0400

    Change vector to const array `CAR_SPRITES`

--- src/render/textures.rs
+++ src/render/textures.rs
@@ -1,5 +1,14 @@
 use macroquad::prelude::*;
 
+const CAR_SPRITES: [&str; 6] = [
+    "assets/cars/Car_Sprite_Sheet_Resized.png",
+    "assets/cars/Audi_Sprite_Sheet.png",
+    "assets/cars/Mini_Truck_Sprite_Sheet.png",
+    "assets/cars/Mini_Van_Sprite_Sheet.png",
+    "assets/cars/Taxi_Sprite_Sheet.png",
+    "assets/cars/Viper_Sprite_Sheet.png",
+];
+
 #[derive(PartialEq, Clone)]
 pub struct Textures {
     pub road: Texture2D,
@@ -10,26 +19,14 @@ pub struct Textures {
 impl Textures {
     pub async fn load() -> Self {
         let mut cars = Vec::new();
-        // Load multiple car textures, you can add more paths as needed
-        let car_paths = vec![
-            "assets/cars/Car_Sprite_Sheet_Resized.png",
-            "assets/cars/Audi_Sprite_Sheet.png",
-            "assets/cars/Mini_Truck_Sprite_Sheet.png",
-            "assets/cars/Mini_Van_Sprite_Sheet.png",
-            "assets/cars/Taxi_Sprite_Sheet.png",
-            "assets/cars/Viper_Sprite_Sheet.png",
-        ];
-
-        for path in car_paths {
-            cars.push(macroquad::texture::load_texture(path).await.unwrap());
+        for sprite in CAR_SPRITES {
+            cars.push(load_texture(sprite).await.unwrap())
         }
         Self {
-            road: macroquad::texture::load_texture("assets/asphalt/ground_asphalt_synth_08.png")
+            road: load_texture("assets/asphalt/ground_asphalt_synth_08.png")
                 .await
                 .unwrap(),
-            bg: macroquad::texture::load_texture("assets/RockBG.png")
-                .await
-                .unwrap(),
+            bg: load_texture("assets/RockBG.png").await.unwrap(),
             cars,
         }
     }
